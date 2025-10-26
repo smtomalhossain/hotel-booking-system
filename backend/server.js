14e39484e740aca7e090535989bf9ea8b94d9a5e
Tomal Hossain <tomalhossain84@gmail.com>
2025-10-26 21:39:31 +0600

    First Commit

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import "dotenv/config";
+import cors from 'cors';
+import connectDB from './configs/db.js';
+import { clerkMiddleware } from '@clerk/express'
+import clerkWebhook from './controllers/clerkWebhooks.js';
+
+connectDB()
+
+const app = express();
+app.use(cors());
+
+//Middleware
+app.use(express.json())
+app.use(clerkMiddleware())
+
+
+//API to listen to Clerk Webhooks
+app.use("/api/clerk", clerkWebhook);
+
+app.get('/', (req, res) => {
+    res.send('Api Is Working');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
